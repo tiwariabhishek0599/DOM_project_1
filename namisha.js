11136cd68979578eefa5852380d4c2d53663b698
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2023-10-03 22:58:35 +0530

    Created

---
+++ namisha.js
@@ -0,0 +1,9 @@
+let divBox = document.getElementById("divv");
+divBox.style.backgroundColor = "yellow";
+divBox.style.color = "blue";
+divBox.style.padding = "10px";
+divBox.style.fontSize = "18px";
+divBox.style.fontWeight = "bold";
+divBox.style.margin = "20px";
+divBox.style.width = "300px";
+divBox.style.height = "200px";
